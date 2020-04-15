156596ac1b40b0aa0200c1177722376483be42fe
TrueAbastien <bastien.legoy@gmail.com>
2020-04-15 09:54:50 -0400

    Working Documentation

--- src/travail_pratique_3/Travail_Pratique_3.java
+++ src/travail_pratique_3/Travail_Pratique_3.java
@@ -5,6 +5,8 @@
  */
 package travail_pratique_3;
 
+import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -12,12 +14,22 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.geometry.Point2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCodeCombination;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.media.AudioClip;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
@@ -26,7 +38,8 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
- *
+ * Classe Principale - Travail_Pratique_3
+ * Complete l'application principale JavaFX par un jeu de reflexion... et de boules.
  * @author carm
  */
 public class Travail_Pratique_3 extends Application {
@@ -34,6 +47,9 @@ public class Travail_Pratique_3 extends Application {
     private static final int W = 6;
     private static final int H = 6;
     private static final int SIZE = 100;
+    private static final int HEADER_HEIGHT = 35;
+    
+    private List<AudioClip> scoringClips;
 
     private Color[] colors = new Color[] {
             Color.BLACK, Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW
@@ -43,28 +59,57 @@ public class Travail_Pratique_3 extends Application {
     private List<Boule> Tab_Boules;
 
     private IntegerProperty score = new SimpleIntegerProperty();
-
+    
+    /**
+     * Creation du contenu initial de la scène de jeu.
+     * @return Scene de jeu sous forme abstraite
+     */
     private Parent createContent() {
-        Pane root = new Pane();
-        root.setPrefSize(W * SIZE + 150, H * SIZE);
+        BorderPane root = new BorderPane();
+        root.setPrefSize(W * SIZE, H * SIZE + 100);
 
         Tab_Boules = IntStream.range(0, W * H)
                 .mapToObj(i -> new Point2D(i % W, i / W))
                 .map(Boule::new)
                 .collect(Collectors.toList());
+        scoringClips = Arrays.asList(
+                new AudioClip(new File("ressources/audio/win1.mp3").toURI().toString()),
+                new AudioClip(new File("ressources/audio/win2.mp3").toURI().toString()),
+                new AudioClip(new File("ressources/audio/win3.mp3").toURI().toString()));
 
         root.getChildren().addAll(Tab_Boules);
 
         Text textScore = new Text();
-        textScore.setTranslateX(W * SIZE);
-        textScore.setTranslateY(100);
+        textScore.setTranslateX(W * SIZE / 4);
+        textScore.setTranslateY(H * SIZE + 90);
         textScore.setFont(Font.font(68));
         textScore.textProperty().bind(score.asString("Score: [%d]"));
+        
+        MenuItem restartItem = new MenuItem("Restart");
+        restartItem.setOnAction(e -> {
+            score.set(0);
+            Tab_Boules.forEach(Boule::randomize);
+        });
+        restartItem.setAccelerator(new KeyCodeCombination(KeyCode.R, KeyCombination.CONTROL_ANY));
+        MenuItem quitItem = new MenuItem("Quit");
+        quitItem.setOnAction(e -> Platform.exit());
+        quitItem.setAccelerator(new KeyCodeCombination(KeyCode.ESCAPE));
+        Menu toolbar = new Menu("File", null, restartItem, quitItem);
+        Menu authorItem = new Menu("Author", null, new MenuItem("Bastien LEGOY"));
+        MenuItem linkItem = new MenuItem("Repository");
+        linkItem.setOnAction(e -> this.getHostServices().showDocument("https://github.com/TrueAbastien/Travail_Pratique_3"));
+        Menu help = new Menu("?", null, authorItem, linkItem);
+        VBox menuBar = new VBox(new MenuBar(toolbar, help));
+        menuBar.setPrefHeight(HEADER_HEIGHT);
+        root.setTop(menuBar);
 
         root.getChildren().add(textScore);
         return root;
     }
-
+    
+    /**
+     * Verifie l'etat de la grille de jeu.
+     */
     private void checkState() {
         Map<Integer, List<Boule>> rows = Tab_Boules.stream().collect(Collectors.groupingBy(Boule::getRow));
         Map<Integer, List<Boule>> columns = Tab_Boules.stream().collect(Collectors.groupingBy(Boule::getColumn));
@@ -73,37 +118,61 @@ public class Travail_Pratique_3 extends Application {
         columns.values().forEach(this::checkCombo);
     }
 
+    /**
+     * Recherche la totalite des combos effectues dans une direction donnee.
+     * @param Tab_BoulesLine Tableau des boules present dans la direction analysee
+     */
     private void checkCombo(List<Boule> Tab_BoulesLine) {
         Boule jewel = Tab_BoulesLine.get(0);
         long count = Tab_BoulesLine.stream().filter(j -> j.getColor() != jewel.getColor()).count();
         if (count == 0) {
             score.set(score.get() + 1000);
             Tab_BoulesLine.forEach(Boule::randomize);
+            scoringClips.get(new Random().nextInt(scoringClips.size())).play(.5);
         }
     }
-
+    
+    /**
+     * Echange les effets d'une boule avec une autre.
+     * @param a Boule d'origine
+     * @param b Cible de l'echange
+     */
     private void swap(Boule a, Boule b) {
         Paint color = a.getColor();
         a.setColor(b.getColor());
         b.setColor(color);
     }
-
+    
+    /**
+     * Demmare l'application dans son Thread.
+     * @param primaryStage Stage principal de l'application
+     * @throws Exception Relancement des erreurs de creation de l'application
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setScene(new Scene(createContent()));
         primaryStage.show();
     }
-
+    
+    /**
+     * Classe Boule
+     * Representation d'une boule sur la grille de jeu.
+     * @author carm
+     */
     private class Boule extends Parent {
         private Circle circle = new Circle(SIZE / 2);
-
+        
+        /**
+         * Constructeur initialisant les caracteristiques et evenements lies a une boule dans le jeu.
+         * @param point Position de la boule creee dans la grille de jeu
+         */
         public Boule(Point2D point) {
             circle.setCenterX(SIZE / 2);
             circle.setCenterY(SIZE / 2);
             circle.setFill(colors[new Random().nextInt(colors.length)]);
 
             setTranslateX(point.getX() * SIZE);
-            setTranslateY(point.getY() * SIZE);
+            setTranslateY(point.getY() * SIZE + HEADER_HEIGHT);
             getChildren().add(circle);
 
             setOnMouseClicked(event -> {
@@ -117,28 +186,52 @@ public class Travail_Pratique_3 extends Application {
                 }
             });
         }
-
+        
+        /**
+         * Donne a une boule une valeure aleatoire.
+         */
         public void randomize() {
             circle.setFill(colors[new Random().nextInt(colors.length)]);
         }
-
+        
+        /**
+         * Recherche l'index de la colonne auquelle la boule appartient.
+         * @return Index resultant de la recherche
+         */
         public int getColumn() {
-            return (int)getTranslateX() / SIZE;
+            return (int)(getTranslateX() - HEADER_HEIGHT) / SIZE;
         }
-
+        
+        /**
+         * Recherche l'index de la ligne auquelle la boule appartient.
+         * @return Index resultant de la recherche
+         */
         public int getRow() {
-            return (int)getTranslateY() / SIZE;
+            return (int)(getTranslateY() - HEADER_HEIGHT) / SIZE;
         }
-
+        
+        /**
+         * Change la couleur de la boule.
+         * @param color Nouvelle couleur appliquee
+         */
         public void setColor(Paint color) {
             circle.setFill(color);
         }
-
+        
+        /**
+         * Donne la couleur actuelle de la boule.
+         * @return Couleur actuelle
+         */
         public Paint getColor() {
             return circle.getFill();
         }
     }
-
+    
+    /**
+     * Fonction Principale
+     * Initialise l'application et lance son fonctionnement.
+     * @param args Arguments d'execution
+     */
     public static void main(String[] args) {
         launch(args);
     }
